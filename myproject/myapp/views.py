e59499a9d3a0278acbcace0562e18ebc2305e820
Your Name <you@example.com>
2018-03-22 22:57:50 -0400

    new index

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,16 +6,16 @@ def hello(request):
    return render(request, 'myapp/template/index.html', {})
 
 def logged(request):
-   return render(request, 'myapp/template/index.html', {'name':"Brandy",'is_logged_in':True})
+   return render(request, 'myapp/template/index.html', {'name':"Jessica",'is_logged_in':True})
 
 def library(request):
-	return render(request, 'myapp/template/library.html', {'name':"Brandy",'is_logged_in':True})
+	return render(request, 'myapp/template/library.html', {'name':"Jessica",'is_logged_in':True})
 
 def predict(request):
-	return render(request, 'myapp/template/predict.html', {'name':"Brandy",'is_logged_in':True})
+	return render(request, 'myapp/template/predict.html', {'name':"Jessica",'is_logged_in':True})
 
 def history(request):
-	return render(request, 'myapp/template/history.html', {'name':"Brandy",'is_logged_in':True})
+	return render(request, 'myapp/template/history.html', {'name':"Jessica",'is_logged_in':True})
 
 def prediction(request):
-	return render(request, 'myapp/template/prediction.html', {'name':"Brandy",'is_logged_in':True})
+	return render(request, 'myapp/template/prediction.html', {'name':"Jessica",'is_logged_in':True})
